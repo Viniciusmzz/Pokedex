810ebe3f62dc2119037305e59e064bda1df175ee
183574 <alexandreviinicius0118@gmail.com>
2023-06-11 22:31:52 -0300

    atualizando

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,55 +1,64 @@
-import React, { useEffect } from 'react';
-import Navbar from '../components/Navbar';
-import { Chip, Container, Divider, Paper,  Typography } from "@mui/material";
-import {Box} from "@mui/system";
-import PokemonTable from  "../components/PokemonTable";
-import { useNavigate } from 'react-router-dom';
+import { Chip, Container, Divider, Paper, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import PokemonTable from "../components/PokemonTable";
 
 export const Profile = ({ pokemonData }) => {
-    const {name, sprites, moves} = pokemonData || {};
-    const navigate = useNavigate();
+  const { name, sprites, moves } = pokemonData || {};
+  const navigate = useNavigate();
 
- useEffect(()=>{
-    if (!pokemonData){
-        navigate("/");
+  useEffect(() => {
+    if (!pokemonData) {
+      navigate("/");
     }
- }, [])
-    if (pokemonData){
-        return null;
-    }
+  }, []);
 
-    return (
-        <>
-        <Navbar/>
+  if (!pokemonData) {
+    return null;
+  }
 
-        <Container maxWidth="md">
-<Paper elevation={3}>
-<Box display="flex" flexDirection="column"  alignItems="center" p={5}> 
-<Typography variant="h3">{name}</Typography>
-<Box display="flex" alignItems="center" width="100%" >
-<Box component="img" src={sprites.front_default} width="50%" height="100%"/>
-<PokemonTable pokemonData={pokemonData}/>
-</Box>
-<Box width="100%">
-<Divider>Variações</Divider>
-<Box component="img" src={sprites.front_female} width="30%" height="30%"/>
-<Box component="img" src={sprites.front_shiny} width="30%" height="30%"/>
-<Box component="img" src={sprites.front_shiny_female} width="30%" height="30%"/>
-<Divider>Ataque</Divider>
-
-    <Box textAlign="center" marginTop="16px">
-    {moves.map((moveData, key) => (
-     
-    <Chip key={key} sx={{m:"5px"}} label={moveData.move.name}></Chip>  
-    
-    ))
-}
-</Box>
-</Box>
-</Box>
-</Paper>
-</Container>
-</>
-    );
+  return (
+    <>
+      <Navbar hideSearch />
+      <Container maxWidth="md">
+        <Paper elevation={3}>
+          <Box display="flex" flexDirection="column" alignItems="center" p={5}>
+            <Typography variant="h4">{name}</Typography>
+            <Box
+              display="flex"
+              alignItems="center"
+              width="100%"
+              marginBottom="15px"
+              sx={{
+                flexDirection: {
+                  xs: "column",
+                  md: "row",
+                },
+              }}
+            >
+              <Box component="img" src={sprites.front_default} width="50%" height="100%" />
+              <PokemonTable pokemonData={pokemonData} />
+            </Box>
+            <Box width="100%">
+              <Divider>Variações</Divider>
+              <Box display="flex" justifyContent="space-between">
+                <Box component="img" src={sprites.front_female} width="35%" height="35%" />
+                <Box component="img" src={sprites.front_shiny} width="35%" height="35%" />
+                <Box component="img" src={sprites.front_shiny_female} width="35%" height="35%" />
+              </Box>
+              <Divider>Ataques</Divider>
+              <Box textAlign="center" marginTop="15px">
+                {moves.map((moveData, key) => (
+                  <Chip key={key} sx={{ m: "5px" }} label={moveData.move.name} />
+                ))}
+              </Box>
+            </Box>
+          </Box>
+        </Paper>
+      </Container>
+    </>
+  );
 };
 export default Profile;
